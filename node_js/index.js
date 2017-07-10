1e1080e9d5763c1df9dc905fb65f910f6ec3333f
Marcin Spurgiasz <marcin.spurgiasz@capgemini.com>
2017-07-10 12:23:12 +0200

    - file reader

--- node_js/index.js
+++ node_js/index.js
@@ -1,1 +1,33 @@
-console.log('lala');
+var fs = require('fs');
+var colors = require('colors');
+// fs.writeFile('./text.txt', 'Tekst, który zapiszemy w pliku tekst.txt', function(err) {
+//     if (err) throw err; 
+//     console.log('Zapisano!');
+// });
+
+// fs.readFile('./text.txt', 'utf-8', function(err, data) {
+//     console.log('Dane przed zapisam!'.blue);
+//     console.log(data);
+//     fs.appendFile('./text.txt', '\nA tak wygladaja po zapisie', function(err){
+//         if (err) throw err;
+//         console.log('Zapisano!'.magenta);
+//         fs.readFile('./text.txt', 'utf-8', function(err, data) {
+//             console.log('Dane po zapisie!'.blue);
+//             console.log(data);
+//         });
+//     });
+// });
+
+//co z przypadkiem zagnieżdzenia folderów???
+//co oznacza nowo utworzony plik???
+fs.readdir('./', function(err, files){
+    if (err) throw err;
+    console.log('Rozpoznano plik!'.green);
+    fs.writeFile('./text.txt', files, function(err){
+        if (err) throw err;
+        console.log('Zapisano!'.red);
+        fs.readFile('./text.txt', 'utf-8', function(err, data) {
+            console.log(data);
+        });
+    });  
+});
