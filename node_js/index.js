c2c4a15ec2c2dfb93f2f5104ef4b853d7557d8ca
Marcin Spurgiasz <marcin.spurgiasz@capgemini.com>
2017-07-10 18:22:30 +0200

    - read index.html

--- node_js/index.js
+++ node_js/index.js
@@ -1,1 +1,19 @@
-console.log('lala');
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+server.on('request', function (request, response) {
+    if (request.method === 'GET' && request.url === '/index') {
+        response.writeHead(200, {'Content-Type': "text/html; charset=utf-8"});
+        fs.readFile('./index.html', 'utf-8', function (err, data){
+            response.write(data);
+            response.end();
+        });
+    } else {
+        response.writeHead(200, {'Content-Type': 'image/jpg' });
+        response.write(fs.readFileSync('./cat.jpg'), 'binary')
+        response.end()
+    }
+
+});
+server.listen(9000);
