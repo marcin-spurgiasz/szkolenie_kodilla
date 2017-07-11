d7a87842f945f3d138d09517a7ae7a17cb9d5311
Marcin Spurgiasz <marcin.spurgiasz@capgemini.com>
2017-07-11 13:00:21 +0200

    - after review

--- node_js/index.js
+++ node_js/index.js
@@ -5,7 +5,7 @@ var server = http.createServer();
 server.on('request', function (request, response) {
     if (request.method === 'GET' && request.url === '/index') {
         response.writeHead(200, {'Content-Type': "text/html; charset=utf-8"});
-        fs.readFile('./index.html', 'utf-8', function (err, data){
+        fs.readFile('./index.html', 'utf-8', function (err, data) {
             response.write(data);
             response.end();
         });
