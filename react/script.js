61497a00dea97a7fc63fd1a53091cd1a447a1700
Marcin Spurgiasz <marcin.spurgiasz@capgemini.com>
2017-07-13 18:03:02 +0200

    - components

--- react/script.js
+++ react/script.js
@@ -1,13 +1,13 @@
 var movies = [
     {
         id: 1,
-        title: 'harry potter',
+        title: 'Harry potter',
         desc: 'film o czarodzieju',
         img: './img/potter.jpg'
     },
     {
         id: 2,
-        title: 'król lew',
+        title: 'Król lew',
         desc: 'film o lwie',
         img: './img/lew.jpg'
     },
@@ -25,6 +25,7 @@ var movies = [
     }
 ];
 
+
 var moviesElements = movies.map(function (movie){
     return React.createElement('li', {key:movie.id},
         React.createElement('h2', {}, movie.title),
@@ -33,11 +34,72 @@ var moviesElements = movies.map(function (movie){
     );
 });
 
-var element = 
-    React.createElement('div', {},
-        React.createElement('h1', {}, 'Lista filmów'),
-        React.createElement('ul', {}, moviesElements)
-    );
+var MoviesList = React.createClass({
+    propTypes: {
+        movies: React.PropTypes.array.isRequired
+    },
+    render: function () {
+        var movies = this.props.movies.map(function(movie){
+            return React.createElement(Movie, {key: movie.id, movieElement: movie})
+        });
+        
+        return (
+            React.createElement('div', {},
+                React.createElement('h1', {}, 'Lista Filmów'),
+                React.createElement('ul', {}, movies)
+            )
+        )
+    }
+});
 
-ReactDOM.render(element, document.getElementById('app'));
+var Movie = React.createClass({
+    propTypes: {
+        movieElement: React.PropTypes.object.isRequired
+    },
+    render: function () {
+        return (
+            React.createElement('li', {},
+                React.createElement(MovieTitle, {titleElement: this.props.movieElement.title}),
+                React.createElement(MovieDescription, {descElement: this.props.movieElement.desc}),
+                React.createElement(MovieImg, {imgElement: this.props.movieElement.img})
+           
+            )
+        )
+    }
+});
 
+var MovieTitle = React.createClass({
+    propTypes: {
+        titleElement: React.PropTypes.string.isRequired
+    },
+    render: function () {
+        return (
+            React.createElement('h2', {}, this.props.titleElement)
+        )
+    }
+});
+
+var MovieDescription = React.createClass({
+    propTypes: {
+        descElement: React.PropTypes.string.isRequired
+    },
+    render: function () {
+        return (
+            React.createElement('p', {}, this.props.descElement)
+        )
+    }
+});
+
+var MovieImg = React.createClass({
+    propTypes: {
+        imgElement: React.PropTypes.string.isRequired
+    },
+    render: function () {
+        return (
+            React.createElement('img', {src: this.props.imgElement})
+        )
+    }
+})
+
+var allMovies = React.createElement(MoviesList, {movies: movies});
+ReactDOM.render(allMovies, document.getElementById('app'));
