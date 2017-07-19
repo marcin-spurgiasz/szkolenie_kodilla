726c40fff28a88cf04e884f54e51cab5c43a7ddb
Marcin Spurgiasz <marcin.spurgiasz@capgemini.com>
2017-07-19 20:49:33 +0200

    - function refactor - changed on promises

---
+++ ES6/components/App.js
@@ -0,0 +1,70 @@
+var GIPHY_API_URL = 'https://api.giphy.com',
+GIPHY_PUB_KEY = '9af6d92a7b0d48a4812c800105edda24';
+
+App = React.createClass({
+    getInitialState() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
+    getGif: function(searchingText) {
+        return new Promise((resolve, reject) => {
+            let url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', url);
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    let data = JSON.parse(xhr.responseText).data; // 4.
+                    let gif = {  // 5.
+                        url: data.fixed_width_downsampled_url,
+                        sourceUrl: data.url
+                    };
+                    resolve(gif);
+                }
+            };
+            xhr.send();
+        });
+    },
+    handleSearch: function (searchingText) {
+        this.setState({
+            loading: true
+        });
+        this.getGif(searchingText)
+        .then(gif => {
+            this.setState({
+                loading: false,
+                gif: gif,
+                searchingText: searchingText
+            });
+        });
+        // this.getGif(searchingText, function (gif) {
+        //     this.setState({
+        //         loading: false,
+        //         gif: gif,
+        //         searchingText: searchingText
+        //     });
+        // }.bind(this));
+    },
+    render: function (){
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+
+        return (
+            <div style= {styles}>
+                <h1>Wyszukiwarka GIFow</h1>
+                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <Search onSearch={this.handleSearch} />
+                <Gif 
+                    loading={this.state.loading}
+                    url={this.state.gif.url}
+                    sourceUrl={this.state.gif.sourceUrl}
+                />
+            </div>
+        );
+    }
+});
--- ES6/script.js
+++ ES6/script.js
@@ -1,21 +1,2 @@
-//zad1
-const hello = 'hello';
-const world = 'world';
-
-console.log(`${hello} ${world}`);
-
-//zad2
-const multiply = (a, b = 1) => {return a * b};
-console.log(multiply(2));
-
-//zad3
-const average = (...args) => console.log(args.reduce((a, b) => a + b) / args.length);
-
-//zad4
-const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-console.log(average(...grades));
-
-//zad5
-const array = [1, 4, 'Iwona', false, 'Nowak'];
-const [, , firstName, , lastName] = array;
-console.log(`${firstName} ${lastName}`);
+var element = React.createElement(App);
+ReactDOM.render(element, document.getElementById('app'));
