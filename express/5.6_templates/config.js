876bc67c00582e7ec6c80899112a27534d2e3ef5
Marcin Spurgiasz <marcin.spurgiasz@capgemini.com>
2017-08-06 19:16:07 +0200

    - express - Task 5

---
+++ express/5.6_templates/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '485309506355-d0k3bvhj9jdgsf7ue028pjomalabuegt.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'sfR8N-Dent4A1ICUQY30C6-3',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
--- express/5.6_templates/server.js
+++ express/5.6_templates/server.js
@@ -1,19 +1,19 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
+var googleProfile = {};
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.get('/first-view', (req, res) => {
-    res.render('first-view');
-});
-
 app.get('/login', (req, res) => {
-    res.render('content');
+    res.render('login');
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.render('index', { user: req.user});
 });
 
 app.get('/userForm', (req, res) => {
@@ -24,8 +24,51 @@ app.get('/userForm', (req, res) => {
     res.end(JSON.stringify(response));
 });
 
+app.get('/logged', function (req, res) {
+    res.render('logged', { user: googleProfile });
+});
+
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback/',
+    passport.authenticate('google', {
+        successRedirect: '/logged',
+        failureRedirect: '/'
+    })
+);
+
 app.listen(3000);
 
 app.use((req, res, next) => {
     res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+    },
+    (accessToken, refreshToken, profile, cb) => {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }    
+));
+
